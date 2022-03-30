b8d1f03aa394d2ac52d36221cd2f19b9b2e160ff
Balan Artiom <artiombalan331@gmail.com>
2022-03-30 16:31:42 +0300

    compare results

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "benchmark/benchmark.h"
 #include "heap_sort.h"
+#include "insertion_sort.h"
 #include "merge_ins_sort.h"
 #include "merge_sort.h"
 #include "quick_sort.h"
@@ -18,10 +19,21 @@ template <class... Args> void Sort(benchmark::State &state, Args &&... args) {
 
 int min_size = 100;
 int max_size = 100000;
-BENCHMARK_CAPTURE(Sort, Merge_sort, merge_sort)->Range(min_size, max_size);
-BENCHMARK_CAPTURE(Sort, Merge_ins_sort, merge_ins_sort)
+int mul = 1000;
+/* BENCHMARK_CAPTURE(Sort, Insertion_sort, insertion_sort) */
+/*     ->RangeMultiplier(mul) */
+/*     ->Range(min_size, max_size); */
+BENCHMARK_CAPTURE(Sort, Merge_sort, merge_sort)
+    ->RangeMultiplier(mul)
     ->Range(min_size, max_size);
-BENCHMARK_CAPTURE(Sort, Quick_sort, quick_sort)->Range(min_size, max_size);
-BENCHMARK_CAPTURE(Sort, Heap_sort, heap_sort)->Range(min_size, max_size);
+BENCHMARK_CAPTURE(Sort, Merge_ins_sort, merge_ins_sort)
+    ->RangeMultiplier(mul)
+    ->Range(min_size, max_size);
+BENCHMARK_CAPTURE(Sort, Quick_sort, quick_sort)
+    ->RangeMultiplier(mul)
+    ->Range(min_size, max_size);
+BENCHMARK_CAPTURE(Sort, Heap_sort, heap_sort)
+    ->RangeMultiplier(mul)
+    ->Range(min_size, max_size);
 
 BENCHMARK_MAIN();
--- makefile
+++ makefile
@@ -1,6 +1,12 @@
 run: bench
 	./main
 
+json: bench
+	./main --benchmark_out=results.json --benchmark_out_format=json
+
 bench: main.c *.h
 	g++ main.c -std=c++11 -isystem benchmark/include \
 	-Lbenchmark/build/src -lbenchmark -lpthread -o main
+
+compare:
+	~/Programs/google-benchmark/tools/compare.py filters results.json Sort/Merge_sort Sort/Merge_ins_sort

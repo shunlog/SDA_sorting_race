a2da14832efdc981660a8ea64c7b52b33718e391
Balan Artiom <artiombalan331@gmail.com>
2022-03-31 15:29:41 +0300

    run for longer

--- main.c
+++ main.c
@@ -21,29 +21,37 @@ template <class... Args> void Sort(benchmark::State &state, Args &&... args) {
 int min_size = 10000000;
 int max_size = 10000000;
 int mul = 100;
+int min_time = 10;
 /* BENCHMARK_CAPTURE(Sort, Insertion_sort, insertion_sort) */
 /*     ->RangeMultiplier(mul) */
 /*     ->Range(min_size, max_size); */
 BENCHMARK_CAPTURE(Sort, Merge_sort, merge_sort)
     ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
+    ->Range(min_size, max_size)
+    ->MinTime(min_time);
 BENCHMARK_CAPTURE(Sort, Merge_ins_sort, merge_ins_sort)
     ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
-BENCHMARK_CAPTURE(Sort, Kalamaghin_Merge_sort, kalamaghin_merge_sort)
-    ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
+    ->Range(min_size, max_size)
+    ->MinTime(min_time);
+/* BENCHMARK_CAPTURE(Sort, Kalamaghin_Merge_sort, kalamaghin_merge_sort) */
+/*     ->RangeMultiplier(mul) */
+/*     ->Range(min_size, max_size) */
+/*     ->MinTime(min_time); */
 BENCHMARK_CAPTURE(Sort, Quick_sort, quick_sort)
     ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
+    ->Range(min_size, max_size)
+    ->MinTime(min_time);
 BENCHMARK_CAPTURE(Sort, Quick_ins_sort, quick_ins_sort)
     ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
+    ->Range(min_size, max_size)
+    ->MinTime(min_time);
 BENCHMARK_CAPTURE(Sort, Heap_sort, heap_sort)
     ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
-BENCHMARK_CAPTURE(Sort, Kalamaghin_Heap_sort, kalamaghin_heap_sort)
-    ->RangeMultiplier(mul)
-    ->Range(min_size, max_size);
+    ->Range(min_size, max_size)
+    ->MinTime(min_time);
+/* BENCHMARK_CAPTURE(Sort, Kalamaghin_Heap_sort, kalamaghin_heap_sort) */
+/*     ->RangeMultiplier(mul) */
+/*     ->Range(min_size, max_size) */
+/*     ->MinTime(min_time); */
 
 BENCHMARK_MAIN();
